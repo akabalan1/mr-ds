229dcee757dc493f46383735338ab332892cb4e0
akabalan1 <alkabalan@icloud.com>
2025-03-31 17:48:57 -0500

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,8 @@
 
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001', {
-  transports: ['websocket'],
+const socket = io('https://mr-ds-3.onrender.com', {
+  transports: ['websocket']
 });
 
 export default socket;
