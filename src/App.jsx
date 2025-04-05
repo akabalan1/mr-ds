676f7952096b3da8d92acbc425b073081324cc94
akabalan1 <alkabalan@icloud.com>
2025-04-05 13:57:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// src/App.jsx
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import AdminHome from "./pages/AdminHome";
@@ -5,6 +7,7 @@ import AdminMajority from "./pages/AdminMajority";
 import PlayerMajority from "./pages/PlayerMajority";
 import AdminKahoot from "./pages/AdminKahoot";
 import PlayerKahoot from "./pages/PlayerKahoot";
+import PlayerJoin from "./pages/PlayerJoin"; // Add PlayerJoin
 import { GameProvider } from "./GameContext";
 
 export default function App() {
@@ -12,7 +15,8 @@ export default function App() {
     <GameProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/admin" />} />
+          <Route path="/" element={<Navigate to="/join" />} />
+          <Route path="/join" element={<PlayerJoin />} /> {/* PlayerJoin route */}
           <Route path="/admin" element={<AdminHome />} />
           <Route path="/admin/majority" element={<AdminMajority />} />
           <Route path="/admin/kahoot" element={<AdminKahoot />} />
