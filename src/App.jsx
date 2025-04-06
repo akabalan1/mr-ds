df0d3e0672f22ec6e33b18f1a3a87d67456e1643
akabalan1 <alkabalan@icloud.com>
2025-04-06 14:34:15 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-// App.jsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PlayerJoin from './pages/PlayerJoin';
-import Waiting from './pages/Waiting';
-import PlayerKahoot from './pages/PlayerKahoot';
-import PlayerMajority from './pages/PlayerMajority';
-import AdminHome from './pages/AdminHome';
-import AdminKahoot from './pages/AdminKahoot';
-import AdminMajority from './pages/AdminMajority';
-import Results from './pages/Results';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GameProvider } from "./GameContext";
+import AdminHome from "./pages/AdminHome";
+import AdminKahoot from "./pages/AdminKahoot";
+import AdminMajority from "./pages/AdminMajority";
+import PlayerJoin from "./pages/PlayerJoin";
+import Waiting from "./pages/Waiting";
+import PlayerKahoot from "./pages/PlayerKahoot";
+import PlayerMajority from "./pages/PlayerMajority";
+import Results from "./pages/Results";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/join" element={<PlayerJoin />} />
-        <Route path="/waiting" element={<Waiting />} />
-        <Route path="/play/kahoot" element={<PlayerKahoot />} />
-        <Route path="/play/majority" element={<PlayerMajority />} />
-        <Route path="/admin" element={<AdminHome />} />
-        <Route path="/admin/kahoot" element={<AdminKahoot />} />
-        <Route path="/admin/majority" element={<AdminMajority />} />
-        <Route path="/results" element={<Results />} />
-      </Routes>
-    </BrowserRouter>
+    <GameProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/join" element={<PlayerJoin />} />
+          <Route path="/waiting" element={<Waiting />} />
+          <Route path="/play/kahoot" element={<PlayerKahoot />} />
+          <Route path="/play/majority" element={<PlayerMajority />} />
+          <Route path="/admin" element={<AdminHome />} />
+          <Route path="/admin/kahoot" element={<AdminKahoot />} />
+          <Route path="/admin/majority" element={<AdminMajority />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </BrowserRouter>
+    </GameProvider>
   );
 }
 
