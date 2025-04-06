3dfe3ca37203c6335aa22e2437404c1202ecef28
akabalan1 <alkabalan@icloud.com>
2025-04-06 15:27:15 -0500

    Update GameContext.jsx

--- src/GameContext.jsx
+++ src/GameContext.jsx
@@ -1,7 +1,6 @@
 // src/GameContext.jsx
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { io } from "socket.io-client";
-import { useNavigate } from "react-router-dom";
 
 const socket = io("https://mr-ds.onrender.com");
 
@@ -16,8 +15,6 @@ export function GameProvider({ children }) {
   const [mode, setMode] = useState("majority");
   const [questions, setQuestions] = useState([]);
   const [leaderboard, setLeaderboard] = useState([]);
-
-  const navigate = useNavigate();
 
   useEffect(() => {
     const storedName = localStorage.getItem("playerName");
@@ -42,18 +39,12 @@ export function GameProvider({ children }) {
       } else {
         setStep(state.currentQuestionIndex);
       }
-
-      if (state.step === -1 || state.currentQuestionIndex === -1) {
-        localStorage.removeItem("playerName");
-        setPlayerName("");
-        navigate("/join");
-      }
     });
 
     return () => {
       socket.disconnect();
     };
-  }, [navigate]);
+  }, []);
 
   const addPlayer = (name) => {
     socket.emit("player-join", name);
