45dedbc13052b58113db11084d2e302a75b0bf6a
akabalan1 <alkabalan@icloud.com>
2025-04-08 15:31:26 -0500

    Update AdminKahoot.jsx

--- src/pages/AdminKahoot.jsx
+++ src/pages/AdminKahoot.jsx
@@ -136,27 +136,29 @@ export default function AdminKahoot() {
               </p>
               <p style={{ color: "gray" }}>⏳ Time remaining: {timer}s</p>
               <ul style={{ listStyle: "none", padding: 0, display: "flex", justifyContent: "center", gap: "2rem" }}>
-                {currentQuestion.options.map((option, i) => (
-                  <li key={i} style={{ textAlign: "center" }}>
-                    {currentQuestion.rapidFire ? (
-                      <div className="flex flex-col items-center">
-                        <img
-                          src={option === "Sarah" ? "/sarah.png" : "/danish.png"}
-                          alt={option}
-                          style={{ width: "50px", height: "50px", borderRadius: "50%" }}
-                        />
-                        <span style={{ marginTop: "0.5rem", fontWeight: "bold" }}>{option}</span>
-                      </div>
-                    ) : (
-                      <span>{option}</span>
-                    )}
-                  </li>
-                ))}
-              </ul>
+                {currentQuestion.rapidFire ? (
+  <ul style={{ listStyle: "none", padding: 0, display: "flex", justifyContent: "center", gap: "2rem" }}>
+    {currentQuestion.options.map((option, i) => (
+      <li key={i} style={{ textAlign: "center" }}>
+        <div className="flex flex-col items-center">
+          <img
+            src={option === "Sarah" ? "/sarah.png" : "/danish.png"}
+            alt={option}
+            style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+          />
+          <span style={{ marginTop: "0.5rem", fontWeight: "bold" }}>{option}</span>
+        </div>
+      </li>
+    ))}
+  </ul>
+) : (
+  <ul style={{ listStyle: "disc", paddingLeft: "1.5rem" }}>
+    {currentQuestion.options.map((option, i) => (
+      <li key={i} style={{ marginBottom: "0.5rem" }}>{option}</li>
+    ))}
+  </ul>
+)}
 
-
-            </>
-          )}
 
           {step !== "done" && (
   <>
